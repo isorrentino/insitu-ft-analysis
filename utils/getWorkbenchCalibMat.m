62e2882401c734a42bf9114598ebf3af2401d52c
Francisco <ing.andrade.francisco@gmail.com>
2018-09-10 15:35:06 +0200

    modification due to production introducing the code iit folders, now it also copies the file when found to a local folder

--- utils/getWorkbenchCalibMat.m
+++ utils/getWorkbenchCalibMat.m
@@ -1,11 +1,26 @@
 function [calibMatrix]=getWorkbenchCalibMat(pathFile,serialNumber)
     if (exist(strcat(pathFile,'matrix_',serialNumber,'.txt'),'file')==2)
-        calibMatrix=readCalibMat(strcat(pathFile,'matrix_',serialNumber,'.txt'));
+        calibMatrix=readCalibMat(strcat(pathFile,'matrix_',serialNumber,'.txt'));        
     else
+        destinationFile=strcat(pathFile,'matrix_',serialNumber,'.txt');
         disp(strcat({'getRawData: Calibration Matrix '},serialNumber,{' not found in the specified folder. Trying in default folders'}))
         if (exist(strcat('external/ftSensCalib/software/sensAquisitionArchive/',serialNumber,'/','matrix_',serialNumber,'.txt'),'file')==2)
             calibMatrix=readCalibMat(strcat('external/ftSensCalib/software/sensAquisitionArchive/',serialNumber,'/','matrix_',serialNumber,'.txt'));
+            sourceFile=strcat('external/ftSensCalib/software/sensAquisitionArchive/',serialNumber,'/','matrix_',serialNumber,'.txt');
         else
-            disp(strcat({'getRawData: Calibration Matrix '},serialNumber,{' not found in the default folder.'}))
+            if (exist(strcat('external/ftSensCalib/software/sensAquisitionArchive/cod_iit_12491/',serialNumber,'/','matrix_',serialNumber,'.txt'),'file')==2)
+                calibMatrix=readCalibMat(strcat('external/ftSensCalib/software/sensAquisitionArchive/cod_iit_12491/',serialNumber,'/','matrix_',serialNumber,'.txt'));
+                sourceFile=strcat('external/ftSensCalib/software/sensAquisitionArchive/cod_iit_12491/',serialNumber,'/','matrix_',serialNumber,'.txt');
+            else
+                if (exist(strcat('external/ftSensCalib/software/sensAquisitionArchive/cod_iit_683/',serialNumber,'/','matrix_',serialNumber,'.txt'),'file')==2)
+                    calibMatrix=readCalibMat(strcat('external/ftSensCalib/software/sensAquisitionArchive/cod_iit_683/',serialNumber,'/','matrix_',serialNumber,'.txt'));
+                    sourceFile=strcat('external/ftSensCalib/software/sensAquisitionArchive/cod_iit_683/',serialNumber,'/','matrix_',serialNumber,'.txt');
+                else
+                    disp(strcat({'getRawData: Calibration Matrix '},serialNumber,{' not found in the default folder.'}))
+                end
+            end
         end
+    end    
+    if exist('sourceFile','var')
+        copyfile(sourceFile,destinationFile);
     end
