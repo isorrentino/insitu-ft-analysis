69c0986ddd70ce4ed9e32a2f5afacc71fd1c9119
Francisco <ing.andrade.francisco@gmail.com>
2018-09-10 15:34:17 +0200

    now it reads any name of the calibration file not only the lambda name

--- utils/readGeneratedCalibMatrices.m
+++ utils/readGeneratedCalibMatrices.m
@@ -1,4 +1,4 @@
-function [cMat,secMat,WorkbenchMat,extraCoeff]=readGeneratedCalibMatrices(experimentNames,scriptOptions,sensorsToAnalize,names2use,lambdasNames)
+function [cMat,secMat,WorkbenchMat,extraCoeff]=readGeneratedCalibMatrices(experimentNames,scriptOptions,sensorsToAnalize,names2use,calibrationFileNames)
 
 for i=1:length(experimentNames)
 
@@ -20,17 +20,17 @@ for i=1:length(experimentNames)
 
     cMat.(names2use{1})=WorkbenchMat; %first dataset to compare is the orignal workbench
 
-    for lam=1:length(lambdasNames)
-        (names2use{(i-1)*length(lambdasNames)+1+lam})
+    for lam=1:length(calibrationFileNames)
+        (names2use{(i-1)*length(calibrationFileNames)+1+lam})
         for j=1:length(sensorsToAnalize)
             sIndx= find(strcmp(ftNames,sensorsToAnalize{j}));
-            cMat.(names2use{(i-1)*length(lambdasNames)+1+lam}).(sensorsToAnalize{j}) = readCalibMat(strcat(prefixDir,'data/',experimentNames{i},'/calibrationMatrices/',input.calibMatFileNames{sIndx},lambdasNames{lam}));
-            secMat.(names2use{(i-1)*length(lambdasNames)+1+lam}).(sensorsToAnalize{j})= cMat.(names2use{(i-1)*length(lambdasNames)+1+lam}).(sensorsToAnalize{j})(1:6,1:6)/WorkbenchMat.(sensorsToAnalize{j});
-            if size(cMat.(names2use{(i-1)*length(lambdasNames)+1+lam}).(sensorsToAnalize{j}),2)>6
-                extraCoeff.(names2use{(i-1)*length(lambdasNames)+1+lam}).(sensorsToAnalize{j})=cMat.(names2use{(i-1)*length(lambdasNames)+1+lam}).(sensorsToAnalize{j})(:,7:end);
-                cMat.(names2use{(i-1)*length(lambdasNames)+1+lam}).(sensorsToAnalize{j})=cMat.(names2use{(i-1)*length(lambdasNames)+1+lam}).(sensorsToAnalize{j})(1:6,1:6);
+            cMat.(names2use{(i-1)*length(calibrationFileNames)+1+lam}).(sensorsToAnalize{j}) = readCalibMat(strcat(prefixDir,'data/',experimentNames{i},'/calibrationMatrices/',input.calibMatFileNames{sIndx},calibrationFileNames{lam}));
+            secMat.(names2use{(i-1)*length(calibrationFileNames)+1+lam}).(sensorsToAnalize{j})= cMat.(names2use{(i-1)*length(calibrationFileNames)+1+lam}).(sensorsToAnalize{j})(1:6,1:6)/WorkbenchMat.(sensorsToAnalize{j});
+            if size(cMat.(names2use{(i-1)*length(calibrationFileNames)+1+lam}).(sensorsToAnalize{j}),2)>6
+                extraCoeff.(names2use{(i-1)*length(calibrationFileNames)+1+lam}).(sensorsToAnalize{j})=cMat.(names2use{(i-1)*length(calibrationFileNames)+1+lam}).(sensorsToAnalize{j})(:,7:end);
+                cMat.(names2use{(i-1)*length(calibrationFileNames)+1+lam}).(sensorsToAnalize{j})=cMat.(names2use{(i-1)*length(calibrationFileNames)+1+lam}).(sensorsToAnalize{j})(1:6,1:6);
             else
-                extraCoeff.(names2use{(i-1)*length(lambdasNames)+1+lam}).(sensorsToAnalize{j})=zeros(6,1);                
+                extraCoeff.(names2use{(i-1)*length(calibrationFileNames)+1+lam}).(sensorsToAnalize{j})=zeros(6,1);                
             end
         end
     end
