4ab3d37b2fd457e0bea9a7b087ce4a2a290a31e2
Francisco <ing.andrade.francisco@gmail.com>
2018-09-20 12:02:13 +0200

    missing ; to avoid undesired messages

--- utils/generateEstimationTypeNames.m
+++ utils/generateEstimationTypeNames.m
@@ -3,13 +3,13 @@ estimationTypeNames{length(estimationTypes)}='';
   for namingIndex=1:length(estimationTypes)
         switch estimationTypes(namingIndex)
             case 1
-                name='_S'%'_sphere_offset';
+                name='_S';%'_sphere_offset';
             case 2
-                name='_Cm'%'_noMeanOnMain_offset';
+                name='_Cm';%'_noMeanOnMain_offset';
             case 3
-                name='_C'%'_noMean_offset';
+                name='_C';%'_noMean_offset';
             case 4
-                name='_O'%'_oneShot_offset';
+                name='_O';%'_oneShot_offset';
         end
         if useTemperatureBooleans(namingIndex)
             name=strcat(name,'wT');
